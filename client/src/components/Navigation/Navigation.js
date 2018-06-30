748f883ab588a17b48bb6eb0cf10185f0e3aff5c
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-07-01 01:37:57 +0200

    Fix links in sidebar

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -4,11 +4,11 @@ import './Navigation.css';
 
 const Navigation = (props) => (
   <div className='navigation'>
-    <NavLink exact to='/' className='navigation__link' activeClassName='navigation__link--active'><span className="fas fa-home"></span><span className="alt">Home</span></NavLink>
-    <NavLink exact to='/about' className='navigation__link' activeClassName='navigation__link--active'><span className='far fa-user'></span><span className="alt">O mnie</span></NavLink>
-    <NavLink exact to='/skills' className='navigation__link' activeClassName='navigation__link--active'><span className='far fa-code'></span><span className="alt">Umiejętności</span></NavLink>
-    <NavLink exact to='/cv' className='navigation__link' activeClassName='navigation__link--active'><span className='far fa-file-alt'></span><span className="alt">CV</span></NavLink>
-    <NavLink exact to='/contact' className='navigation__link' activeClassName='navigation__link--active'><span className='far fa-comment'></span><span className="alt">Kontakt</span></NavLink>
+    <NavLink exact to='/personal-site' className='navigation__link' activeClassName='navigation__link--active'><span className="fas fa-home"></span><span className="alt">Home</span></NavLink>
+    <NavLink exact to='/personal-site/about' className='navigation__link' activeClassName='navigation__link--active'><span className='far fa-user'></span><span className="alt">O mnie</span></NavLink>
+    <NavLink exact to='/personal-site/skills' className='navigation__link' activeClassName='navigation__link--active'><span className='far fa-code'></span><span className="alt">Umiejętności</span></NavLink>
+    <NavLink exact to='/personal-site/cv' className='navigation__link' activeClassName='navigation__link--active'><span className='far fa-file-alt'></span><span className="alt">CV</span></NavLink>
+    <NavLink exact to='/personal-site/contact' className='navigation__link' activeClassName='navigation__link--active'><span className='far fa-comment'></span><span className="alt">Kontakt</span></NavLink>
   </div>
 );
 
