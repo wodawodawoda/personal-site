10b01543d7c2c3af4e816c4b6353b7ea9eca9f73
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-07-22 03:33:10 +0200

    Fix sidebar nav icons

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = (props) => (
   <div className='navigation'>
     <NavLink exact to='/personal-site' className='navigation__link' activeClassName='navigation__link--active'><span className="fas fa-home"></span><span className="alt">Home</span></NavLink>
     <NavLink exact to='/personal-site/about' className='navigation__link' activeClassName='navigation__link--active'><span className='far fa-user'></span><span className="alt">O mnie</span></NavLink>
-    <NavLink exact to='/personal-site/skills' className='navigation__link' activeClassName='navigation__link--active'><span className='far fa-code'></span><span className="alt">Umiejętności</span></NavLink>
+    <NavLink exact to='/personal-site/skills' className='navigation__link' activeClassName='navigation__link--active'><span className='fas fa-code'></span><span className="alt">Umiejętności</span></NavLink>
     <NavLink exact to='/personal-site/cv' className='navigation__link' activeClassName='navigation__link--active'><span className='far fa-file-alt'></span><span className="alt">CV</span></NavLink>
     <NavLink exact to='/personal-site/contact' className='navigation__link' activeClassName='navigation__link--active'><span className='far fa-comment'></span><span className="alt">Kontakt</span></NavLink>
   </div>
